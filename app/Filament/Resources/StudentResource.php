2630e9a0cca63315c2a5459e4e71a6f766573824
moodloi <86982646+moodloi@users.noreply.github.com>
2024-09-23 22:17:03 +0400

    Student Resource

---
+++ app/Filament/Resources/StudentResource.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StudentResource\Pages;
+use App\Filament\Resources\StudentResource\RelationManagers;
+use App\Models\Student;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Imports\ProductImporter;
+use Filament\Forms\Components\Select;
+use Filament\Actions\ImportAction;
+
+class StudentResource extends Resource
+{
+    protected static ?string $model = Student::class;
+
+    protected static ?string $navigationIcon = 'heroicon-m-user-group';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('first_name')
+                    ->required(),
+                Forms\Components\TextInput::make('last_name')
+                    ->required(),
+                Forms\Components\TextInput::make('pid')
+                    ->required(),
+                Forms\Components\DatePicker::make('date_of_birth')
+                    ->required(),
+               Select::make('gender')
+                ->options([
+                    'male' => 'male',
+                    'female' => 'female',
+
+                ]),
+                Forms\Components\Textarea::make('comment')
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('first_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('last_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('pid')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('date_of_birth')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('gender')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStudents::route('/'),
+            'create' => Pages\CreateStudent::route('/create'),
+            'edit' => Pages\EditStudent::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/StudentResource/Pages/ListStudents.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\StudentResource\Pages;
+
+use App\Filament\Resources\StudentResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+use App\Filament\Imports\StudentImporter;
+use Filament\Actions\ImportAction;
+
+class ListStudents extends ListRecords
+{
+    protected static string $resource = StudentResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+            Actions\ImportAction::make()
+                ->importer(StudentImporter::class),
+        ];
+    }
+}
