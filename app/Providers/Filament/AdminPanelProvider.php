08f5de60439ce11faad1567856bca8e5a1e7e317
moodloi <86982646+moodloi@users.noreply.github.com>
2024-10-25 01:37:09 +0400

    Sidebar Hidable

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('mng')
+            ->sidebarCollapsibleOnDesktop()
             ->login()
             ->databaseNotifications()
             ->colors([
